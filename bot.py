4de62c72e35cf141ff42eef5ec700ab19f7c0caf
hanbee95 <nospicynocilantro@gmail.com>
2022-06-18 02:01:58 -0500

    change guild not to specify

--- bot.py
+++ bot.py
@@ -12,15 +12,16 @@ from datetime import datetime
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+#GUILD = os.getenv('DISCORD_GUILD')
 ctxg = 0
 def time_module():
     print("time module in use")
     while True:
-        current_time = datetime.now().strftime("%H:%M")#hour %H min %M sec %S am:pm %p 
+        #print (datetime.now())
+        current_time = datetime.now().strftime("%m/%d/%Y, %H:%M")#hour %H min %M sec %S am:pm %p 
         #print (current_time)###
         #print (ctxg)
-        if current_time == "00:00": # enter the time you wish 
+        if current_time == "06/18/2022, 02:15": # enter the time you wish 
             if ctxg != 0:
                 print ("send")
                 asyncio.run_coroutine_threadsafe(to_yuumi(ctxg), bot.loop)
@@ -32,6 +33,16 @@ bot = commands.Bot(command_prefix='&', description='Happy Birthday Alarm')
 
 @bot.event
 async def on_ready():
+    #HAN From here to guild fetching
+    GUILD = []
+    async for guild_fetch in bot.fetch_guilds(limit=150):
+        GUILD.append(guild_fetch.name)    
+    print(f"Allowed servers: {GUILD}")
+    #HAN to here to guild fetching
+
+    for guild in client.guilds:
+        print("Was added to the servers list")
+
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
@@ -41,7 +52,7 @@ async def on_ready():
 @bot.command()
 async def pre(ctx):
     """storing ctx"""
-    await ctx.send("prepared")
+    await ctx.send("-"*20)
     global ctxg 
     ctxg = ctx
     loop = asyncio.new_event_loop()
